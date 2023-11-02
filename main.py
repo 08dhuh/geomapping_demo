71e916faa5026ff38e3e4cdf779fb6ab2fc6c536
Doran Huh <08dhuh@gmail.com>
2023-11-03 08:40:54 +1100

    minor changes

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-longitude = 146.4511
-latitude = -38.2324
 
-df = pd.DataFrame(np.array([[longitude, latitude]]), 
-                  columns=['longitude', 'latitude'])
+def display_map(longitude=None,latitude=None):
+       longitude = longitude or 146.4511
+       latitude = latitude or -38.2324
 
-st.map(df,latitude=latitude,
-       longitude=longitude,
-       size=0,
-       zoom=9)
+       df = pd.DataFrame(np.array([[longitude, latitude]]), 
+                     columns=['longitude', 'latitude'])
+
+       st.map(df,latitude=latitude,
+              longitude=longitude,
+              size=0,
+              zoom=9)
+
+if __name__=='__main__':
+       display_map()
